c59160162a386ba6bcc454cbf78fb3735c52477a
August Miller <hello@gusmiller.com>
2023-05-25 09:31:40 -0700

    Reset project state
    This brings it into agreement with the existing starter project, minus a thing or two that we’ll replace anyway

--- webpack.mix.js
+++
@@ -1,8 +0,0 @@
-// load mix
-let mix = require("laravel-mix");
-
-// compile src/styles.css → web/styles.css
-mix.postCss("src/styles.css", "web/", [
-  require("postcss-nested"),
-  require("tailwindcss")
-]);
